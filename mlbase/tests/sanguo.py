12f467920d5b50ede996aecc5f7b48cc22516edd
Xutop <906344661@qq.com>
2017-05-09 20:45:07 +0800

    test rnns

--- mlbase/tests/sanguo.py
+++ mlbase/tests/sanguo.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 30 10:38:27 2016
-
-@author: xtop
-"""
  
 import numpy as np
 import mlbase.network as N
@@ -11,80 +6,41 @@ import mlbase.layers.activation as act
 import mlbase.load_sanguo as l
 from mlbase.layers import *
 import mlbase.cost as cost
+import theano.tensor as T
 
 path = '/hdd/home/largedata/sanguoyanyi'
 x = l.word_number(path)
 z = l.sort_dict(x)
 ws = l.random_embedding_init(z,100)
 
-def rnn_sanguo2():
-    network = N.Network()
-    network.debug = True
-    #network.saveInterval = 10
-    network.learningRate = 0.001
-    network.costFunction = cost.ImageSSE
-    network.bantchsize = 50
-    network.setInput(RawInput((1, 100,1)))
-    network.append(reshape.Flatten())
-    network.append(rfc_no_out.R_fullConn(input_feature=100, h_num=500, output_feature=100))
-    network.append(rfc_no_in.R_fullConn(input_feature=100, h_num=500, output_feature=100))
-    network.build()
-
-    dataset = l.load_snaguo2(path, ws)
-    trX = dataset[:5000].reshape(-1, 1, 100, 1)
-    trY = dataset[:5000]
-    teX = dataset[-300:].reshape(-1, 1, 100, 1)
-    teY = dataset[-300:]
-
-    for i in range(5000):
-        print(i)
-        network.train(trX, trY)
-        print('train end')
-        #if i%20 == 0 and i!=0:
-            #t = l.values2keys(ws, teY)
-            #l.values2keys(ws, network.predict(teX))
-            #true_value = np.asarray(t)
-            #pre_value = np.asarray(p)
-            #print(1 - np.mean(true_value == pre_value))
-        print(1 - np.mean(np.asarray(l.values2keys(ws, teY)) == np.asarray(l.values2keys(ws, network.predict(teX)))))
-        print('----------------------------')
-        print(l.values2keys(ws, teY)[:30])
-        print(l.values2keys(ws, network.predict(teX))[:30])
-
-
-def rnn_sanguo():
+def rnn():
     network = N.Network()
     network.debug = True
     #network.saveInterval = 20
-    network.learningRate = 0.001
+    network.learningRate = 0.01
     network.costFunction = cost.ImageSSE
-    network.bantchsize = 50
+    #network.batchSize = 5
     network.setInput(RawInput((1, 100,1)))
     network.append(reshape.Flatten())
-    network.append(r_fullconn.R_fullConn(input_feature=100, h_num=500, output_feature=100))
-    #network.append(r_fullconn.R_fullConn(input_feature=100, h_num=500, output_feature=100))
+    #network.append(bi_gru.BiGRU(input_dim=100, hidden_dim=500))
+    network.append(gru.GRU(input_dim=100, hidden_dim=500))
+    #network.append(rnn.RNN(input_feature=100, h_num=500))
     network.build()
     
     dataset = l.load_snaguo2(path, ws)
-    trX = dataset[:3000].reshape(-1, 1, 100, 1)
-    trY = dataset[:3000]
-    teX = dataset[-300:].reshape(-1, 1, 100, 1)
-    teY = dataset[-300:]
+    trX = dataset[:1000].reshape(-1, 1, 100, 1)
+    trY = dataset[:1000]
+    teX = dataset[-500:].reshape(-1, 1, 100, 1)
+    teY = dataset[-500:]
 
     for i in range(5000):
         print(i)
         network.train(trX, trY)
         print('train end')
-        #if i%20 == 0 and i!=0:
-            #t = l.values2keys(ws, teY)
-            #l.values2keys(ws, network.predict(teX))
-            #true_value = np.asarray(t)
-            #pre_value = np.asarray(p)
-            #print(1 - np.mean(true_value == pre_value))
         print(1 - np.mean(np.asarray(l.values2keys(ws, teY)) == np.asarray(l.values2keys(ws, network.predict(teX)))))
         print('----------------------------')
-        print(l.values2keys(ws, teY)[:30])
-        print(l.values2keys(ws, network.predict(teX))[:30])
+        print(l.values2keys(ws, teY)[:50])
+        print(l.values2keys(ws, network.predict(teX))[:50])
         
 if __name__ == '__main__':
-    rnn_sanguo()
+    rnn()
